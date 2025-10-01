4c2bc2ed3f39e3b286c5abafd7e6041812b2b373
kaza452027 <1543252798@qq.com>
2025-10-01 14:20:20 +0800

    Update SecurityConfig.java
    securityconfig change web site name

--- GalWeb/galend/src/main/java/com/gal/galend/security/SecurityConfig.java
+++ GalWeb/galend/src/main/java/com/gal/galend/security/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://127.0.0.1:5500","http://localhost:5500"));
+        cfg.setAllowedOrigins(List.of("https://galweb.pages.dev","http://127.0.0.1:5500","http://localhost:5500"));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
@@ -95,5 +95,6 @@ public class SecurityConfig {
         return source;
     }
 }
+
 
 
